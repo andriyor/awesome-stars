245d86fd56c0035cee207d88b0d675c64d66d04d
Heng-Yi Wu <henry40408@gmail.com>
2019-03-31 12:17:45 +0800

    style: add return type according to eslint

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -2,11 +2,11 @@ import { browser } from "webextension-polyfill-ts";
 
 import { RATE_LIMIT } from "./constants";
 
-async function onRuntimeInstalled(details) {
+async function onRuntimeInstalled(details): Promise<void> {
     console.log("previous version", details.previousVersion);
 }
 
-async function onTabsUpdated(tabId, changeInfo, tab) {
+async function onTabsUpdated(tabId, changeInfo, tab): Promise<void> {
     if (~tab.url.indexOf("github.com")) {
         await browser.pageAction.show(tabId);
     }
@@ -17,7 +17,6 @@ browser.contextMenus.create({
     title: "Rate Limit: 0 / 5,000",
     contexts: ["page_action"]
 });
-
 
 browser.runtime.onInstalled.addListener(onRuntimeInstalled);
 browser.tabs.onUpdated.addListener(onTabsUpdated);
